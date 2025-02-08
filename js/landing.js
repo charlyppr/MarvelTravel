33994f6b6012aaf3ad935b08471ecb6337154b08
Charly <pupierchar@cy-tech.fr>
2025-02-09 02:42:54 +0300

    suppression des commentaires

--- js/landing.js
+++ js/landing.js
@@ -45,27 +45,23 @@ function rotatePierres() {
         const deltaX = distanceX / (5 + moveFactor);
         const deltaY = distanceY / (5 + moveFactor);
 
-        // Logique d'incrémentation ou de décrémentation de l'angle
         if (incrementing) {
-            rotationAngle += 0.03; // Augmenter l'angle
+            rotationAngle += 0.03;
             if (rotationAngle >= 1000) {
-                incrementing = false; // Une fois qu'on atteint 3600, on commence à décrémenter
+                incrementing = false;
             }
         } else {
-            rotationAngle -= 0.03; // Réduire l'angle
+            rotationAngle -= 0.03;
             if (rotationAngle <= 0) {
-                incrementing = true; // Une fois qu'on atteint 0, on recommence à incrémenter
+                incrementing = true;
             }
         }
 
-        // Appliquer la transformation de translation et de rotation
         pierre.style.transform = `translate(${deltaX}px, ${deltaY}px) rotate(${rotationAngle}deg)`;
     });
 
-    // Demander au navigateur de rappeler cette fonction pour une animation fluide
     requestAnimationFrame(rotatePierres);
 }
 
-// Démarrer l'animation de rotation
 rotatePierres();
 
