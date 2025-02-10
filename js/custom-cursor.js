0fcf88e96a22bbafca4c8d4d969c8f47257b95b2
Charly <pupierchar@cy-tech.fr>
2025-02-10 05:54:35 +0300

    refonte de l'accueil : amélioration du style des cartes, ajustement des polices et mise à jour du curseur personnalisé

--- js/custom-cursor.js
+++ js/custom-cursor.js
@@ -1,4 +1,4 @@
-const cursor = document.getElementById('custom-cursor');
+const cursor = document.querySelector('.default');
 
 let cursorX = 0, cursorY = 0;
 let inactivityTimeout;
@@ -41,3 +41,102 @@ document.addEventListener('mousemove', (e) => {
 });
 
 updateCursor();
+
+
+
+
+const styleSheet = document.createElement('style');
+document.head.appendChild(styleSheet);
+
+styleSheet.sheet.insertRule(`
+    .default::before {
+        content: "";
+        font-size: 0em;
+        transition: font-size 0.3s ease-out;
+    }
+`, styleSheet.sheet.cssRules.length);
+
+document.querySelectorAll('.github-ibra').forEach(function (link) {
+    link.addEventListener('mouseenter', function () {
+        // Lorsque le curseur entre sur un lien avec la classe 'github-ibra'
+        document.querySelector('.default').classList.add('link-hover');
+        
+        // Modifie le contenu et la taille de la police pour le lien 'ibra'
+        styleSheet.sheet.insertRule(`
+            .default::before {
+                content: "Ibra";
+                font-size: 1em;
+            }
+        `, styleSheet.sheet.cssRules.length);
+    });
+
+    link.addEventListener('mouseleave', function () {
+        // Lorsque le curseur quitte le lien avec la classe 'github-ibra'
+        document.querySelector('.default').classList.remove('link-hover');
+        
+        // Restaure la taille de la police à 0em lorsque le curseur quitte le lien
+        styleSheet.sheet.insertRule(`
+            .default::before {
+                content: "";
+                font-size: 0em;
+            }
+        `, styleSheet.sheet.cssRules.length);
+    });
+});
+
+document.querySelectorAll('.github-paul').forEach(function (link) {
+    link.addEventListener('mouseenter', function () {
+        // Lorsque le curseur entre sur un lien avec la classe 'github-paul'
+        document.querySelector('.default').classList.add('link-hover');
+        
+        // Modifie le contenu et la taille de la police pour le lien 'paul'
+        styleSheet.sheet.insertRule(`
+            .default::before {
+                content: "Paul";
+                font-size: 1em;
+            }
+        `, styleSheet.sheet.cssRules.length);
+    });
+
+    link.addEventListener('mouseleave', function () {
+        // Lorsque le curseur quitte le lien avec la classe 'github-paul'
+        document.querySelector('.default').classList.remove('link-hover');
+        
+        // Restaure la taille de la police à 0em lorsque le curseur quitte le lien
+        styleSheet.sheet.insertRule(`
+            .default::before {
+                content: "";
+                font-size: 0em;
+            }
+        `, styleSheet.sheet.cssRules.length);
+    });
+});
+
+document.querySelectorAll('.github-charly').forEach(function (link) {
+    link.addEventListener('mouseenter', function () {
+        // Lorsque le curseur entre sur un lien avec la classe 'github-paul'
+        document.querySelector('.default').classList.add('link-hover');
+        
+        // Modifie le contenu et la taille de la police pour le lien 'paul'
+        styleSheet.sheet.insertRule(`
+            .default::before {
+                content: "Charly";
+                font-size: 1em;
+            }
+        `, styleSheet.sheet.cssRules.length);
+    });
+
+    link.addEventListener('mouseleave', function () {
+        // Lorsque le curseur quitte le lien avec la classe 'github-paul'
+        document.querySelector('.default').classList.remove('link-hover');
+        
+        // Restaure la taille de la police à 0em lorsque le curseur quitte le lien
+        styleSheet.sheet.insertRule(`
+            .default::before {
+                content: "";
+                font-size: 0em;
+            }
+        `, styleSheet.sheet.cssRules.length);
+    });
+});
+
--- js/scroll-to.js
+++ js/scroll-to.js
@@ -7,3 +7,38 @@ moreContainer.addEventListener('click', function() {
         block: 'center'
     });
 });
+
+
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+        let rect = card.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top; 
+        
+        let centerX = rect.width / 2;
+        let centerY = rect.height / 2;
+        
+        let rotateX = (y - centerY) / 20;
+        let rotateY = (centerX - x) / 20;
+
+        let shadowX = (centerX - x) / 25;
+        let shadowY = (centerY - y) / 25;
+    
+        
+        card.style.transition = `transform 0.2s linear`;
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        card.style.boxShadow = `${-shadowX}px ${-shadowY}px 20px #f2dbaf30`;
+
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+        card.style.transition = `transform 0.2s linear, box-shadow 1s ease-in-out`;
+        setTimeout(() => {
+            card.style.boxShadow = `0px 0px 20px #f2dbaf30`;
+        }, 100);
+    });
+});
