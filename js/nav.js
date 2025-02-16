daf60d0a0e40277847c70563f51e41b380ec735a
Charly <pupierchar@cy-tech.fr>
2025-02-16 21:50:58 +0300

    ajout de requêtes média pour améliorer la réactivité, ajustements de styles pour les petits écrans et modifications de la navigation

--- js/nav.js
+++ js/nav.js
@@ -3,31 +3,35 @@ const navBar = document.querySelector('.nav');
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
-    let scrollTop = document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        navBar.style.transform = 'translateY(-100%)';
-    } else {
-        navBar.style.transform = 'translateY(0)';
+    if (window.innerWidth > 768) {
+        let scrollTop = document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            navBar.style.transform = 'translateY(-100%)';
+        } else {
+            navBar.style.transform = 'translateY(0)';
+        }
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
     }
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const menuItems = document.querySelectorAll('.menu-li');
+    if (window.innerWidth > 768) {
+        const menuItems = document.querySelectorAll('.menu-li');
 
-    menuItems.forEach(item => {
-        item.addEventListener('mouseover', () => {
-            menuItems.forEach(otherItem => {
-                if (otherItem !== item) {
-                    otherItem.classList.add('hovered');
-                }
+        menuItems.forEach(item => {
+            item.addEventListener('mouseover', () => {
+                menuItems.forEach(otherItem => {
+                    if (otherItem !== item) {
+                        otherItem.classList.add('hovered');
+                    }
+                });
+            });
+
+            item.addEventListener('mouseout', () => {
+                menuItems.forEach(otherItem => {
+                    otherItem.classList.remove('hovered');
+                });
             });
         });
-
-        item.addEventListener('mouseout', () => {
-            menuItems.forEach(otherItem => {
-                otherItem.classList.remove('hovered');
-            });
-        });
-    });
+    }
 });
