f83299758755e4895700df1ebf2809cf8b050a66
Charly <pupierchar@cy-tech.fr>
2025-02-18 17:59:50 +0300

    ajout de marges et ajustement des styles des filtres dans destination.css ; mise à jour du traitement des données du formulaire dans traitement.php pour inclure le mot de passe

--- traitement.php
+++ traitement.php
@@ -1,15 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Vérification de l'existence des clés dans $_POST
-    $nom = isset($_POST['nom']) ? htmlspecialchars($_POST['nom']) : '';
-    $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
-    $message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
 
-    // Affichage des informations soumises
+    $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+    $password = isset($_POST['password']) ? htmlspecialchars($_POST['password']) : '';
+
+
     echo "<h2>Informations soumises :</h2>";
-    echo "<p><strong>Nom :</strong> " . $nom . "</p>";
     echo "<p><strong>Email :</strong> " . $email . "</p>";
-    echo "<p><strong>Message :</strong> " . $message . "</p>";
+    echo "<p><strong>Mot de passe :</strong> " . $password . "</p>";
 } else {
     echo "Aucune donnée soumise.";
 }
